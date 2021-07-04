4be62f00dfc1a5560cbb3b328a74c290f612de59
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2021-07-04 20:48:28 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
+import useStyles from "./style";
+
+const cards = [1,2,3,4,5,6,7,8,9];
+
+function App() {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar positon="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm" style={{ marginTop: "160px" }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is a photo album and I'm trying to make this
+              sentence as long as posible so we can see how does it look like on
+              this screen.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card)=> (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to
+                      destription the content.
+                    </Typography>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          
+          </Grid>
+        </Container>
+      </main>
+    </>
+  );
+}
+
+export default App;
